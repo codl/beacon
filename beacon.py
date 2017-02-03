8b56e27a406b7ff6cc935abe8c7fcc1e9f8f09b6
codl <codl@codl.fr>
2017-02-03 01:32:30 +0100

    let browsers cache the preflight

--- beacon.py
+++ beacon.py
@@ -10,7 +10,7 @@ from util import genid
 logging.getLogger("elasticsearch").setLevel(logging.ERROR)
 
 app = Flask("beacon")
-CORS(app)
+CORS(app, send_wildcard=True, max_age=60*60*24*365)
 
 INDEX_PREFIX = getenv("BEACON_PREFIX", "beacon-")
 
