15dc261878866f46d076cc3fff6134d7cab69fc3
codl <codl@codl.fr>
2019-03-06 03:16:26 +0100

    add authentication finally
    also got rid of all that web tracking stuff i wasnt using
    
    also made a thing for upgrading the database schema

--- beacon.py
+++ beacon.py
@@ -1,16 +1,12 @@
 from flask import Flask, request, send_file, make_response, render_template
 from flask_cors import CORS
-from os import getenv
-import psycopg2
 import json
 import time
+from db import setup_db, get_pg
 
 app = Flask("beacon")
 CORS(app, max_age=60*60*24*365, supports_credentials=True)
 
-
-def get_pg():
-    return psycopg2.connect(getenv("BEACON_POSTGRESQL", ""), application_name="beacon")
 
 @app.route("/favicon.ico")
 def favicon():
@@ -24,72 +20,55 @@ def robots():
 def index():
     return render_template("index.html")
 
-@app.route("/s")
-def script():
-    return send_file("script.js")
+def is_authed(request, cur):
+    authenticated = False
+
+    auth = request.headers.get('authorization', '')
+    if auth.startswith('Bearer '):
+        auth = auth[7:]
+        cur.execute('''SELECT FROM auth_tokens WHERE token = %s''', (auth,))
+        if cur.fetchone() is not None:
+            authenticated = True
+
+    return authenticated
 
 
-@app.route("/collect/<path:path>", methods={"GET", "POST"})
+@app.route("/collect/<path:path>", methods={"POST",})
 def recieve_beacon(path=""):
+    if not (request.is_json or request.content_type == "application/csp_report"):
+        return "this isn't json", 415
 
-    params = list(request.values.items())
-    if len(params) > 0:
-        body = dict(params)
-    elif request.is_json or request.content_type == "application/csp_report":
-        body = request.get_json(force = True);
+    body = request.get_json(force = True);
 
     collected_at = time.time()
     if 'collected_at' in body:
         collected_at = body['collected_at']
         del body['collected_at']
 
-    body = json.dumps(body)
 
     pg = get_pg()
     cur = pg.cursor()
 
+    authenticated = is_authed(request, cur)
+
+    body = json.dumps(body)
+
     success = True
     try:
         cur.execute("""
-            INSERT INTO beacons (collected_at, type, body)
-            VALUES (to_timestamp(%(collected_at)s), %(type)s, %(body)s)
-            ON CONFLICT (type, collected_at, body) DO UPDATE SET count = beacons.count + 1, received_at = now();
-        """, dict(collected_at=collected_at, type=path, body=body))
+            INSERT INTO beacons (collected_at, type, body, authenticated)
+            VALUES (to_timestamp(%(collected_at)s), %(type)s, %(body)s, %(authed)s)
+            ON CONFLICT (type, collected_at, body, authenticated) DO UPDATE SET count = beacons.count + 1, received_at = now();
+        """, dict(collected_at=collected_at, type=path, body=body, authed=authenticated))
         pg.commit()
     except Exception as e:
         pg.rollback()
         raise e
         success = False
 
-    if(request.method == "GET"):
-        resp = make_response(send_file("pixel.png", mimetype="image/png",
-            add_etags=False, cache_timeout=0),
-            200 if success else 500)
-    else:
-        resp = make_response("", 204 if success else 500)
+    resp = make_response("", 204 if success else 500)
 
     return resp
-
-def setup_db():
-    pg = get_pg()
-    cur = pg.cursor()
-    cur.execute('''
-        CREATE TABLE IF NOT EXISTS beacons (
-            id serial PRIMARY KEY,
-            collected_at timestamp without time zone DEFAULT now(),
-            received_at timestamp without time zone DEFAULT now(),
-            type text,
-            body jsonb,
-            count integer DEFAULT 1,
-            UNIQUE (type, collected_at, body)
-        );
-        CREATE INDEX IF NOT EXISTS idx_beacons_collected_at ON beacons (collected_at);
-        CREATE INDEX IF NOT EXISTS idx_beacons_type_collected_at ON beacons (type, collected_at);
-        ''')
-    pg.commit()
-
-
-app.before_first_request(setup_db)
 
 if __name__ == "__main__":
     app.run()
---
+++ db.py
@@ -0,0 +1,71 @@
+import psycopg2
+from os import getenv
+
+def get_pg():
+    return psycopg2.connect(
+        getenv("BEACON_POSTGRESQL", ""), application_name="beacon")
+
+
+def setup_db():
+    pg = get_pg()
+    cur = pg.cursor()
+    cur.execute('''
+        SELECT FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'db_versions';
+    ''')
+    if cur.fetchone() is None:
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS beacons (
+                id serial PRIMARY KEY,
+                collected_at timestamp without time zone DEFAULT now(),
+                received_at timestamp without time zone DEFAULT now(),
+                type text,
+                body jsonb,
+                count integer DEFAULT 1,
+                CONSTRAINT beacons_type_collected_at_body_key UNIQUE (type, collected_at, body)
+            );
+            CREATE INDEX IF NOT EXISTS idx_beacons_collected_at ON beacons (collected_at);
+            CREATE INDEX IF NOT EXISTS idx_beacons_type_collected_at ON beacons (type, collected_at);
+            CREATE TABLE IF NOT EXISTS db_versions (
+                version integer PRIMARY KEY
+            );
+            INSERT INTO db_versions VALUES (0);
+            ''')
+        version = 0
+    else:
+        cur.execute('''SELECT version FROM db_versions;''')
+        row = cur.fetchone()
+        if not row:
+            raise Exception('Found DB but could not find DB version.')
+        version = row[0]
+
+    if version < 1:
+        cur.execute('''
+            CREATE TABLE auth_tokens (
+                token text PRIMARY KEY
+            );
+            ALTER TABLE beacons
+                ADD COLUMN authenticated
+                    BOOLEAN
+                    NOT NULL
+                    DEFAULT 'f'
+                ,
+                DROP CONSTRAINT beacons_type_collected_at_body_key
+                ,
+                ADD CONSTRAINT const_uniqueness
+                    UNIQUE (type, collected_at, authenticated, body)
+            ;
+            -- the unique constraint already creates a more detailed index
+            DROP INDEX idx_beacons_type_collected_at;
+        ''')
+        version = 1
+
+    cur.execute(
+        '''
+        DELETE FROM db_versions;
+        INSERT INTO db_versions VALUES (%s);
+        ''', (version, ))
+
+    pg.commit()
+
+if __name__ == '__main__':
+    setup_db()
